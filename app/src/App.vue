03c325b60cf1a11e474d3ae5f8ee3ef2a4b006df
Davi Kawasaki <davishinjik@gmail.com>
2017-10-07 14:19:57 -0300

    Pannel component with slot, props and scss scoped

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,17 +1,26 @@
 <template>
-  <section>
+  <section class="body">
     <!--<h1>{{ title }}</h1>-->
-    <h1 v-text="title"></h1>
-    <ul>
-      <li v-for="photo of photos" :key="photo.title">
-        <img :src="photo.url" :alt="photo.title" />
+    <h1 v-text="title" class="center"></h1>
+    <ul class="list">
+      <li v-for="photo of photos" :key="photo.titulo" class="item">
+        <apic-pannel :title="photo.titulo">
+          <div slot="img">
+            <img :src="photo.url" :alt="photo.titulo" class="img-responsive" />
+          </div>
+        </apic-pannel>
       </li>
     </ul>
   </section>
 </template>
 
 <script>
+import Pannel from './components/shared/pannel/Pannel.vue'
+
 export default {
+  components: {
+    'apic-pannel': Pannel
+  },
   data() {
     return {
       title: 'Alura Pic with VueJS',
@@ -27,5 +36,22 @@ export default {
 </script>
 
 <style lang="scss">
-
+  /* Main */ 
+  .body {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+  .center {
+    text-align: center;
+  }
+  .list {
+    list-style: none;    
+  }
+  .list .item {
+    display: inline;
+  }
+  .img-responsive {
+    width: 100%;
+  }
 </style>
---
+++ app/src/components/shared/pannel/Pannel.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="pannel">
+        <h2 class="pannel-title">{{ title }}</h2>
+        <slot name="img" class="pannel-content"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['title']
+}
+</script>
+
+<style scoped lang="scss">
+  /* Pannel */ 
+  .pannel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .pannel .pannel-title {
+    text-align: center;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
+</style>
