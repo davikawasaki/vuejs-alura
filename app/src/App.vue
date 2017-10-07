d5f6ef20d290ec9e2d196ea55943e073ce29814e
Davi Kawasaki <davishinjik@gmail.com>
2017-10-06 22:52:42 -0300

    Changing app folder + lifecycle hooks with

---
+++ app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <!--<h1>{{ title }}</h1>-->
+    <h1 v-text="title"></h1>
+    <ul>
+      <li v-for="photo of photos" :key="photo.title">
+        <img :src="photo.url" :alt="photo.title" />
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Alura Pic with VueJS',
+      photos: []
+    }
+  },
+  created() {
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(photos => this.photos = photos, err => console.log(err));
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
